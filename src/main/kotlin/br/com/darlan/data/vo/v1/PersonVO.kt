034ee72c502ed187863348fed85a7528caeaf474
gabriel.darlan88@gmail.com <gabriel.darlan88@gmail.com>
2024-02-02 20:24:07 -0300

    capitulo completo

--- src/main/kotlin/br/com/darlan/data/vo/v1/PersonVO.kt
+++ src/main/kotlin/br/com/darlan/data/vo/v1/PersonVO.kt
@@ -1,9 +1,17 @@
 package br.com.darlan.data.vo.v1
 
+import com.fasterxml.jackson.annotation.JsonIgnore
+import com.fasterxml.jackson.annotation.JsonProperty
+import com.fasterxml.jackson.annotation.JsonPropertyOrder
+
+@JsonPropertyOrder("id", "address", "firstName", "lastName", "gender")
 data class PersonVO(
     var id: Long? = 0,
+    @field:JsonProperty("first_name")
     var firstName: String = "",
+    @field:JsonProperty("last_name")
     var lastName: String = "",
     var address: String = "",
+    @field:JsonIgnore
     var gender: String = "",
 )
